90903224af57e5becbb1fa7536ed0000d0183e32
Abhaythakur0o6 <abhaythakur0474@gmail.com>
2026-07-08 16:56:34 +0530

    Containerize project using Docker Compose

--- server/controllers/payment-controller.js
+++ server/controllers/payment-controller.js
@@ -8,7 +8,6 @@ const createPaymentLink = wrapAsync(async (req, res) => {
 
 const updatePaymentInformation = wrapAsync(async (req, res) => {
     const paymentLink = await paymentService.updatePaymentInformation(req.query);
-    console.log("URL IS",process.env.CLIENT_URL);
     return res.redirect(`${process.env.CLIENT_URL}/order-success`);
 })
 
